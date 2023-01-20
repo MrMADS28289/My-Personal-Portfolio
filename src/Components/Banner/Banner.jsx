300fb7400e18fea4360806099bb9db15aebcc433
ADSM <yesform2018@gmail.com>
2023-01-20 20:24:34 +0600

    Navbar Completed With Scroll effect

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,19 +1,20 @@
 import './Banner.css';
 import background from '../../Assets/Videos/background.mp4';
 import me from '../../Assets/Images/me.png';
-import { AiOutlineDownload } from 'react-icons/ai';
+import { AiOutlineDownload, AiOutlineGithub } from 'react-icons/ai';
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 import { Typewriter } from "react-simple-typewriter";
 
 const Banner = () => {
 
     return (
-        <div className='h-[6300px] w-full relative overflow-hidden'>
+        <div className='h-[600px] w-full relative overflow-hidden'>
 
-            <div className='absolute top-0 left-0 h-full w-full bg-[#000000c2]' />
+            <div className='absolute top-0 left-0 h-full w-full bg-[#000000ac]' />
             <video className='video' src={background} loop muted autoPlay />
 
-            <div className='absolute w-full h-full top-0 text-white flex mx-16'>
-                <div className='intro w-[50%] h-full mt-20'>
+            <div className='absolute w-full h-full top-0 text-white flex mx-16 mt-14'>
+                <div className='intro w-[40%] h-full mt-20'>
                     <h1 className='text-3xl text-gray-300'>Hello! I'm</h1>
                     <h3 className='text-4xl mt-3'>
                         <Typewriter
@@ -29,11 +30,47 @@ const Banner = () => {
                         />
                     </h3>
                     <p className='text-xs text-gray-400 mt-6'>MERN Stack Developer, Specializing in UI/UX Developments.</p>
-                    <button className='flex items-center justify-center gap-2 h-[50px] w-[150px] text-gray-300 border border-[#252734] rounded-full hover:bg-[#252734] duration-300 mt-10'>Get Resume <AiOutlineDownload /></button>
+
+                    <button className='flex items-center justify-center gap-2 h-[60px] w-[160px] text-white border border-[#252734] rounded-full hover:bg-[#252734] duration-300 mt-10'>Get Resume <AiOutlineDownload /></button>
                 </div>
 
-                <div className='w-[50%] h-full'>
-                    <img className='absolute bottom-0 max-h-[500px]' src={me} alt="Masum Abduss Sobhan" />
+                <div className='h-full'>
+
+                    <div className='flex absolute bottom-[-20px]'>
+
+                        <div className='h-[500px] w-[500px] flex justify-center border-r border-gray-700 rounded-full'>
+
+                            <div className='h-[400px] w-[400px] flex justify-center border-r border-gray-300 rounded-full mt-10'>
+
+                                <div className='h-[300px] w-[300px] border-r border-gray-700 rounded-full mt-10' />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <a
+                        className='absolute border border-gray-200 rounded-full p-2 mt-[170px] ml-[330px] bg-black hover:bg-white hover:text-black duration-300'
+                        href="https://github.com/MrMADS28289" target="blank">
+                        <AiOutlineGithub />
+                    </a>
+                    <a
+                        className='absolute border border-gray-200 rounded-full p-2 mt-[250px] ml-[410px] bg-black hover:bg-white hover:text-black duration-300'
+                        href="https://www.linkedin.com/in/abduss-sobhan/" target="blank">
+                        <FaLinkedinIn />
+                    </a>
+                    <a
+                        className='absolute border border-gray-200 rounded-full p-2 mt-[360px] ml-[430px] bg-black hover:bg-white hover:text-black duration-300'
+                        href="https://www.facebook.com/Mr.Mads2" target='blank'>
+                        <FaFacebookF />
+                    </a>
+                    <a
+                        className='absolute border border-gray-200 rounded-full p-2 mt-[465px] ml-[390px] bg-black hover:bg-white hover:text-black duration-300'
+                        href="https://www.facebook.com/Mr.Mads2" target='blank'>
+                        <FaTwitter />
+                    </a>
+
+
+                    <img className='absolute bottom-0 max-h-[500px] grayscale-[0.3]' src={me} alt="Masum Abduss Sobhan" />
                 </div>
             </div>
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,40 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import { MdDoubleArrow } from 'react-icons/md';
+import { SiWebmin } from 'react-icons/si';
 import useScrollPosition from '../Hooks/useScrollPosition';
 
 const Navbar = () => {
 
     const [sticky, setSticky] = useState('');
+    const [color, setColor] = useState('');
     const { scrollY } = useScrollPosition();
 
     useEffect(() => {
         if (!scrollY) {
             setSticky('');
+            setColor('');
         }
         if (scrollY) {
-            setSticky('bg-white');
+            setSticky('bg-gray-200 h-[70px]');
+            setColor('text-black');
         }
     }, [scrollY]);
 
-
     return (
-        <div className={`flex justify-between fixed top-0 left-0 mx-8 transition-[0.6s] z-[1000] ${sticky}`}>
-            <a href="#home" className='relative transition-[0.6s]'>
-                <span className='flex items-center gap-2 text-lg font-bold'>
-                    <span className='text-purple-400'>const </span>
-                    <span className='text-red-400'>Me </span>
-                    <span className='text-purple-400'>= </span>
-                    <span className='text-green-400 text-4xl'>Masum </span>
-                    <span className='text-sky-400 text-4xl'>;</span>
+        <div className={`flex justify-between items-center fixed top-0 left-0 px-16 py-6 duration-300 z-[1000] w-full ${sticky} ${color} text-gray-300`}>
+            <a href="#home" className='relative duration-75'>
+                <span className={`flex items-center gap-2 font-bold ${color} text-white text-4xl animate-pulse`}>
+                    <SiWebmin /> Masum
                 </span>
             </a>
 
-            <ul className='flex relative'>
-                <li className='relative'><a className='relative transition-[0.6s]' href="#home">Home</a></li>
-                <li className='relative'><a className='relative transition-[0.6s]' href="#service">Service</a></li>
-                <li className='relative'><a className='relative transition-[0.6s]' href="#blog">Blog</a></li>
-                <li className='relative'><a className='relative transition-[0.6s]' href="#pages">Pages</a></li>
-                <li className='relative'><a className='relative transition-[0.6s]' href="#contact">Contact</a></li>
+            <ul className='flex relative gap-7 font-bold'>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#service">Service</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#pages">Pages</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
+
+            <button className={`flex items-center justify-center gap-2 h-[50px] w-[150px] text-white font-semibold border border-gray-500 rounded-full hover:bg-[#252734] duration-300 ${color}`}>Hire Me <MdDoubleArrow /></button>
         </div>
     );
 };
